7b1d90d004be4f3262c75f3d2b268bb2fce2f3c4
Delinyah Koning <koningdelinyah@gmail.com>
2025-10-17 17:38:59 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// One place for tweakables:
+window.APP = {
+  cursorModes: ['dot','solar','native'],
+  defaultCursorMode: 'dot',
+
+  // Hero liquify parameters
+  hero: {
+    MAX_IMPRINTS: 150,
+    LIFE: 9000,
+    BASE: 128,
+    BASE_RADIUS: 110,
+    STRETCH: 3.0,
+    TWIRL: 1.8,
+    POWER: 1.5
+  }
+};
